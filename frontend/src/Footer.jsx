eac6f29de997bd6e63c8e1a987f74763ef8cbb20
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-01-12 21:58:47 +0545

    Created footer component

---
+++ frontend/src/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+    
+
+    return(
+        <footer className="bg-black text-white text-center py-4 whitespace-nowrap">
+            Copyright &copy; 2026 Kiran Bhattarai
+        </footer>
+    )
+
+
+}
+
+export default Footer
