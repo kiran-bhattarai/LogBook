7a619019d1cf3a311448965914b7946a4ce0577b
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-01-16 17:11:20 +0545

    Created Body element and added paths for experimenting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,23 @@ import SignupPage from "./SignupPage"
 import NoteInput from "./NoteInput"
 import NotesCard from "./NotesCard"
 import NotesView from "./NotesView"
+import Footer from "./Footer"
+import Body from "./Body.jsx"
+import NoteEditor from "./NoteEditor.jsx"
+import ProfilePage from "./Profile.jsx"
+import SearchUsers from "./SearchUsers.jsx"
+import UserItem from "./UserItem.jsx"
 
 function App() {
 
   return (
     <Routes>
+      <Route path="/item" element={<UserItem />} />
+      <Route path="/search" element={<SearchUsers />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/editor" element={<NoteEditor />} />
+      <Route path="/body" element={<Body />} />
+      <Route path="/footer" element={<Footer />} />
       <Route path="/input" element={<NoteInput />} />
       <Route path="/view" element={<NotesView />} />
       <Route path="/card" element={<NotesCard />} />
---
+++ frontend/src/Body.jsx
@@ -0,0 +1,51 @@
+import LoginPage from "./LoginPage"
+import { Routes, Route } from "react-router-dom"
+import ForgotPassword from "./ForgotPassword"
+import SignupPage from "./SignupPage"
+import NoteInput from "./NoteInput"
+import NotesCard from "./NotesCard"
+import NavBar from "./NavBar"
+import NotesView from "./NotesView"
+import Footer from "./Footer"
+import SortBy from "./SortBy"
+import NoteEditor from "./NoteEditor"
+import { useState } from "react"
+
+
+function Body() {
+
+    const [editor, setEditor] = useState(false)
+
+    const [selectedNote, setSelectedNote] = useState(null)
+
+
+    return (
+        <>
+            <div className="min-h-screen flex flex-col bg-neutral-900">
+                <NavBar />
+
+                <div className="p-5 flex justify-center flex-col items-center">
+                    <NoteInput />
+                    <SortBy></SortBy>
+                </div>
+
+                <div className="flex-1 overflow-y-auto p-4 max-w-[95%] mx-auto">
+                        <NotesView onNoteClick={setSelectedNote}/>
+                </div>
+
+
+                <Footer />
+                {selectedNote && <div className="fixed h-full w-full bg-[#00000063] z-10">
+                    <div>
+                    <NoteEditor note={selectedNote}></NoteEditor>
+                    </div>
+                </div>}
+            </div >
+        </>
+    );
+
+
+
+}
+
+export default Body
