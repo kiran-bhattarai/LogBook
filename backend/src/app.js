c23b3fa984fef470d53c928e39ac8a17cca4f04c
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-01-18 20:47:52 +0545

    Added cors, error handler middleware and auth router

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from "cors"
 import dotenv from 'dotenv';
+import authRouter from './routes/auth.route.js';
+import { globalErrorHandler } from './middlewares/global-error-handler.js';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors({origin: process.env.FRONTEND_URL}))
+
+app.use("/auth", authRouter)
+
+app.use(globalErrorHandler)
 
 export default app;
-  
