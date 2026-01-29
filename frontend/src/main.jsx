6bdb0fa8c734ec8f01f15c4b105bb60ca7ca3b7d
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-01-29 09:46:44 +0545

    Added ThemeProvider to the app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import ThemeProvider from './ThemeProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </StrictMode>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+  </StrictMode >
 )
