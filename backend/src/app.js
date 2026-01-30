8e739fd6d3d038c5ffc29ca410887f8e9eac207a
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-01-30 21:03:06 +0545

    Added cookie parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,13 @@ import dotenv from 'dotenv';
 import authRouter from './routes/auth.route.js';
 import noteRouter from "./routes/note.route.js"
 import { globalErrorHandler } from './middlewares/global-error-handler.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors({origin: process.env.FRONTEND_URL, credentials: true}))
+app.use(cookieParser());
 
 app.use("/auth", authRouter)
 app.use("/note", noteRouter)
