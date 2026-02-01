a01d7c07d68de09f7249dcff775f2b5b58702ff5
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-02-01 19:55:44 +0545

    Better logout logic

--- backend/src/controllers/auth/logout.controller.js
+++ backend/src/controllers/auth/logout.controller.js
@@ -3,6 +3,9 @@ import { logout as logoutService } from "../../services/auth/logout.service.js"
 export const logout = async (req, res, next) => {
     const refreshTokenId = req.cookies.refreshToken.split(".")[0]
     const userId = req.user.sub
+
+    res.clearCookie("refreshToken", { httpOnly: true, secure: false, sameSite: "lax" })
+    
     await logoutService(userId, refreshTokenId)
-    res.status(200).json({message: "Logout successful"})
+    res.status(200).json({ message: "Logout successful" })
 }
