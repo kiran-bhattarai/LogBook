8eb8aa9b7be5afa6422310146ae94da2ff76f249
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-02-01 20:40:01 +0545

    Connected components together using states and show different UI if user not authenticated

--- frontend/src/Body.jsx
+++ frontend/src/Body.jsx
@@ -10,17 +10,25 @@ import Footer from "./Footer"
 import SortBy from "./SortBy"
 import NoteEditor from "./NoteEditor"
 import { useState } from "react"
-
+import SearchUsers from "./SearchUsers"
+import { useAuth } from "./AuthProvider"
+import UnauthenticatedPage from "./UnauthenticatedPage"
 
 function Body() {
 
     const [editor, setEditor] = useState(false)
+
+    const { user } = useAuth()
 
     const [sortId, setSortId] = useState(localStorage.getItem("sortId") || 1)
 
     const [selectedNote, setSelectedNote] = useState(null)
 
     const [newNoteCreated, setNewNoteCreated] = useState(0)
+
+    const [searchAccounts, setSearchAccounts] = useState(false)
+
+    const [searchingFor, setSearchingFor] = useState("")
 
     const handleNewNoteCreated = (note) => {
         setNewNoteCreated(prev => prev + 1)
@@ -33,15 +41,23 @@ function Body() {
     }
 
     const setSelectedNoteMain = (value) => {
-        setNewNoteCreated(prev => prev+1)
+        setNewNoteCreated(prev => prev + 1)
         setSelectedNote(value)
+    }
+
+    if (!user) {
+        return (
+            <>
+                <UnauthenticatedPage></UnauthenticatedPage>
+            </>
+        )
     }
 
 
     return (
         <>
             <div className="min-h-screen flex flex-col bg-neutral-900">
-                <NavBar />
+                <NavBar setSearchUsers={setSearchAccounts} setSearchingFor={setSearchingFor} />
 
                 <div className="p-5 flex justify-center flex-col items-center">
                     <NoteInput onNoteCreated={handleNewNoteCreated} />
@@ -50,7 +66,7 @@ function Body() {
 
                 <div className="flex justify-center flex-1">
                     <div className="flex-1 overflow-y-auto p-4 max-w-[95%]">
-                        <NotesView onNoteClick={setSelectedNote} newNoteCreated={newNoteCreated} sortId={sortId} />
+                        <NotesView onNoteClick={setSelectedNote} newNoteCreated={newNoteCreated} sortId={sortId} searchingFor={searchingFor} />
                     </div>
                 </div>
 
@@ -59,6 +75,11 @@ function Body() {
                 {selectedNote && <div className="fixed h-full w-full bg-[#00000063] z-10">
                     <div>
                         <NoteEditor note={selectedNote} setNoteOnScreen={setSelectedNoteMain}></NoteEditor>
+                    </div>
+                </div>}
+                {searchAccounts && <div className="absolute h-full w-full bg-[#00000063] z-10">
+                    <div>
+                        <SearchUsers setSearchUsers={setSearchAccounts} />
                     </div>
                 </div>}
             </div >
