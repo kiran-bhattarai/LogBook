c2a2f7d76edbe9d115e26467868a404796023e2f
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-02-04 19:19:52 +0545

    Connect with backend and better UI/UX

--- frontend/src/SearchUsers.jsx
+++ frontend/src/SearchUsers.jsx
@@ -1,15 +1,37 @@
 import UserItem from "./UserItem"
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
-function SearchUsers({setSearchUsers}) {
+function SearchUsers({ setSearchUsers }) {
 
-    const [users, setUsers] = useState([])
+    const [searchTerm, setSearchTerm] = useState("")
+    const [users, setUsers] = useState(null)
 
     const containerRef = useRef()
 
+    useEffect(() => {
+
+        const fetchUsers = async () => {
+            if (!searchTerm) {
+                setUsers(null)
+                return
+            }
+
+            const timeout = setTimeout(async () => {
+                const res = await fetch(`${import.meta.env.VITE_API_URL}/profile/search?term=${searchTerm}`)
+                const data = await res.json()
+                setUsers(data.foundUsers)
+            }, 400)
+
+            return () => clearTimeout(timeout)
+        }
+
+        fetchUsers()
+
+    }, [searchTerm])
+
     const handleClick = (e) => {
-        if(containerRef && !containerRef.current.contains(e.target)){
+        if (containerRef && !containerRef.current.contains(e.target)) {
             setSearchUsers(false)
         }
     }
@@ -21,12 +43,18 @@ function SearchUsers({setSearchUsers}) {
                 <div className="w-full">
                     <div className="w-full flex items-center justify-center relative text-white">
                         <img src="../src/assets/search_dark.svg" alt="" className="absolute h-6 left-4" />
-                        <input placeholder="Search" className={`bg-transparent py-1.5 m-1 w-full px-10 rounded-4xl  text-xl border-neutral-600 border outline-none text-ellipsis mx-1`} />
+                        <input onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search" className={`bg-transparent py-1.5 m-1 w-full px-10 rounded-4xl  text-xl border-neutral-600 border outline-none text-ellipsis mx-1`} />
                     </div>
                 </div>
-                <div className="p-4 bg-[#1d1d1d] gap-2 flex flex-col h-full rounded mt-2">
-                    {users.map((user) => <UserItem user={user} />)}
+
+                <div className={`p-4 bg-[#1d1d1d] gap-2 flex flex-col h-full rounded mt-2 overflow-auto scrollbar ${!((users?.length !== 0) && (users !== null)) ? "items-center" : ""}`}>
+                    {searchTerm &&
+                        !((users?.length !== 0) && (users !== null)) ?
+                            <div className="text-neutral-400 text-3xl top-1/2 translate-y-1/2 h-[60%]">No user found</div>
+                            :
+                            users?.map((user) => <UserItem setItselfOffOnFalse={setSearchUsers} key={user._id} user={user} />)}
                 </div>
+
             </div>
         </div>
     )
--- frontend/src/UserItem.jsx
+++ frontend/src/UserItem.jsx
@@ -1,13 +1,17 @@
-function UserItem({user}) {
+import { Link } from "react-router-dom"
+
+function UserItem({ user, setItselfOffOnFalse }) {
 
     return (
-        <div className="border-b border-neutral-500 flex items-center bg-black p-2 py-1">
-            <img src={user.picture} alt="" className="bg-neutral-300 h-12 rounded-full mr-4" />
-            <div className="">
-                <h1 className="text-white text-2xl">{user.name}</h1>
-                <h3 className="text-neutral-400">{user.id}</h3>
+        <Link onClick={() => setItselfOffOnFalse(false)} to={`/profile?id=${user._id}`}>
+            <div className="border-b border-neutral-500 flex items-center rounded bg-[#181818] pb-1 px-3">
+                <img src={user.avatar ? user.avatar : "../src/assets/user_profile.png"} alt="" className="bg-neutral-300 h-10 rounded-full mr-2" />
+                <div className="">
+                    <h1 className="text-white text-2xl">{user.name}</h1>
+                    <h3 className="text-neutral-400 text-sm">{user._id}</h3>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
