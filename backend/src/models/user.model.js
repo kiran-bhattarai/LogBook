fbbdc78aeaeb98d653f90ac6d3394b8cd05792d0
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-02-05 20:45:14 +0545

    Small bug fixes

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -23,7 +23,7 @@ const UserSchema = new mongoose.Schema({
     },
     createdAt: {
         type:  Date,
-        default: new Date()
+        default: Date.now
     }
 })
 
--- backend/src/services/auth/refresh-token.service.js
+++ backend/src/services/auth/refresh-token.service.js
@@ -30,7 +30,7 @@ export const generateRefreshToken = async (userId, device = "Unknown", ip = "0.0
 export const validateRefreshToken = async (refreshToken) => {
     const tokenId = refreshToken.split(".")[0]
 
-    const refreshTokenDb = await RefreshToken.findOne({ tokenId })
+    const refreshTokenDb = await RefreshToken.findOneAndDelete({ tokenId })
     if (!refreshTokenDb) {
         throw new AppError("Invalid refresh token", 403)
     }
@@ -41,11 +41,8 @@ export const validateRefreshToken = async (refreshToken) => {
     }
 
     if (refreshTokenDb.expiresAt < new Date()) {
-        await RefreshToken.deleteOne({ tokenId })
         throw new AppError("Refresh token expired", 403)
     }
-
-    await RefreshToken.deleteOne({ tokenId })
 
     const newTokenId = crypto.randomBytes(16).toString("hex")
     const newSecret = crypto.randomBytes(32).toString("hex")
