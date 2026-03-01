61bf5e80699eed644410ab6efacf8b81b1be2583
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-03-01 23:03:30 +0545

    feat: create Spinner component

---
+++ frontend/src/Spinner.jsx
@@ -0,0 +1,11 @@
+function Spinner() {
+
+    return (
+
+        <div className="h-10 w-10 border-4 border-white border-t-transparent rounded-full animate-spin">
+        </div>
+
+    )
+}
+
+export default Spinner
