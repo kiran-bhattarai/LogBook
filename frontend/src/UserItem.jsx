bafafe072150795796ce7c6e9e9c76499b34db76
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-03-01 23:05:31 +0545

    fix: solve google link avatar missing on localhost

--- frontend/src/UserItem.jsx
+++ frontend/src/UserItem.jsx
@@ -5,7 +5,7 @@ function UserItem({ user, setItselfOffOnFalse }) {
     return (
         <Link onClick={() => setItselfOffOnFalse(false)} to={`/profile?id=${user._id}`}>
             <div className="border-b border-neutral-500 flex items-center rounded bg-[#181818] pb-1 px-3">
-                <img src={user.avatar ? user.avatar : "../src/assets/user_profile.png"} alt="" className="bg-neutral-300 h-10 rounded-full mr-2" />
+                <img src={user.avatar ? user.avatar : "../src/assets/user_profile.png"} alt="avatar" referrerPolicy="no-referrer" className="bg-neutral-300 h-10 rounded-full mr-2" />
                 <div className="">
                     <h1 className="text-white text-2xl">{user.name}</h1>
                     <h3 className="text-neutral-400 text-sm">{user._id}</h3>
