ad4db5c3ff0a9ce61ba2220facf75af573320bf2
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-03-13 10:34:31 +0545

    feat(footer): improve UI

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     
     return(
-        <footer className="bg-black text-white text-center py-4 whitespace-nowrap z-5">
+        <footer className="py-3 overflow-hidden bg-black text-neutral-200 font-light text-center text-sm font-poppins shadow-[0px_0px_1px_1px] shadow-[#b1b1b131]">
             Copyright &copy; 2026 Kiran Bhattarai
         </footer>
     )
