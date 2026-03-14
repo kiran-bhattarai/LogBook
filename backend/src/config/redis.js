7b6cf4c4c899fd217e5e6f6ae491adbdfd42e317
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-03-14 22:02:52 +0545

    fix: add extension to import and better error logging

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,12 +1,12 @@
 import { createClient } from "redis"
-import logger from "./logger"
+import logger from "./logger.js"
 
 export const redisClient = createClient({
     url: process.env.REDIS_URL
 })
 
 redisClient.on("error", (err) => {
-    logger.error(err)
+    logger.error("Redis client error:",err)
 })
 
 redisClient.on("connect", () => {
